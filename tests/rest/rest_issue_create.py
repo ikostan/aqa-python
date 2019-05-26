f40d57b172cc781aca86581755de1e9852d43ffb
Dmitry <dmitry.naumov.work@gmail.com>
2019-05-26 19:29:34 +0300

    create issue tests completed

--- tests/rest/rest_issue_create.py
+++ tests/rest/rest_issue_create.py
@@ -6,24 +6,44 @@ class RestIssueCreate(RestIssue):
     def __init__(self, cookie_value):
         super().__init__(cookie_value)
 
-    def rest_jira_create_issue(self, summary, description, issue_type="Bug", assignee_name=None):
+    def rest_jira_create_issue(self, project_key, summary, issue_type_name, description, assignee_name, priority_name):
         url = self.JIRA_DOMAIN + self.JIRA_ISSUE
-        if assignee_name is None: assignee_name = self.JIRA_USER_NAME
         payload = self.dict_to_string(
-            {
-                "fields": {
-                    "project": {
-                        "key": self.JIRA_PROJECT_KEY
-                    },
-                    "summary": summary,
-                    "description": description,
-                    "issuetype": {
-                        "name": issue_type
-                    },
-                    "assignee": {
-                        "name": assignee_name
-                    }
-                }
-            }
-        )
+            self.__get_body(project_key, summary, issue_type_name, description, assignee_name, priority_name))
         return self.req.post(url, data=payload, headers=self.headers)
+
+    def __get_body(self, project_key, summary, issue_type_name, description, assignee_name, priority_name):
+        request_body = dict(fields={})
+        if project_key is not None: request_body["fields"].update(self.__get_body_project(project_key))
+        if summary is not None: request_body["fields"].update(self.__get_body_summary(summary))
+        if issue_type_name is not None: request_body["fields"].update(self.__get_body_issue_type(issue_type_name))
+        if description is not None: request_body["fields"].update(self.__get_body_description(description))
+        if assignee_name is not None: request_body["fields"].update(self.__get_body_assignee(assignee_name))
+        if priority_name is not None: request_body["fields"].update(self.__get_body_priority(priority_name))
+        return request_body
+
+    def __get_body_project(self, project_key):
+        body_section = dict(project={})
+        body_section["project"].update(dict(key=project_key))
+        return body_section
+
+    def __get_body_summary(self, summary):
+        return dict(summary=summary)
+
+    def __get_body_issue_type(self, issue_type_name):
+        body_section = dict(issuetype={})
+        body_section["issuetype"].update(dict(name=issue_type_name))
+        return body_section
+
+    def __get_body_description(self, description):
+        return dict(description=description)
+
+    def __get_body_assignee(self, assignee_name):
+        body_section = dict(assignee={})
+        body_section["assignee"].update(dict(name=assignee_name))
+        return body_section
+
+    def __get_body_priority(self, priority_name):
+        body_section = dict(priority={})
+        body_section["priority"].update(dict(name=priority_name))
+        return body_section
--- tests/test_rest_create.py
+++ tests/test_rest_create.py
@@ -12,14 +12,25 @@ class TestRestCreate:
         return RestIssueCreate(cookie)
 
     @allure.title("REST JIRA. Create")
-    @pytest.mark.parametrize("summary, description, issue_type, assignee_name, status_code", [
-        ("Test Summary 1", "Test Summary 1 Description", "Bug", None, 201),
-        ("Test Summary 2", "Test Summary 2 Description", "Bug", None, 201),
-        ("Test Summary 3", "Test Summary 3 Description", "Bug", None, 201),
-        ("Test Summary 4", "Test Summary 4 Description", "Bug", None, 201),
-    ])
-    def test_create_issue(self, create, summary, description, issue_type, assignee_name, status_code):
+    @pytest.mark.parametrize("project_key, summary, issue_type, description, assignee_name, priority_name, status_code",
+                             [
+                                 ("default", "Test Issue Summary1", "Bug", "Test Issue Description", "default",
+                                  "Medium", 201),
+                                 ("default", "Test Issue Summary2", "Bug", None, None, None, 201),
+                                 ("default", "123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 12345",
+                                  "Bug", None, None, None, 201),
+                                 ("default", "123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456",
+                                  "Bug", None, None, None, 400),
+                                 (None, "Test Issue Summary5", "Bug", None, None, None, 400),
+                                 ("default", None, "Bug", None, None, None, 400),
+                                 ("default", "Test Issue Summary7", None, None, None, None, 400),
+                             ])
+    def test_create_issue(self, create, project_key, summary, issue_type, description, assignee_name, priority_name,
+                          status_code):
+        with allure.step("Setup request"):
+            if project_key == "default": project_key = create.JIRA_PROJECT_KEY
+            if assignee_name == "default": assignee_name = create.JIRA_USER_NAME
         with allure.step("Send request"):
-            r = create.rest_jira_create_issue(summary, description, issue_type, assignee_name)
+            r = create.rest_jira_create_issue(project_key, summary, issue_type, description, assignee_name, priority_name)
         with allure.step("Check the status code"):
             assert r.status_code == status_code
